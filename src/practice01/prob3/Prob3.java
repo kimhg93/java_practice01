05c7a3b7e2bcca322c9eb6dc04875b21120c4107
defaultuser0 <defaultuser0@DESKTOP-FDVUPSA>
2019-08-06 17:02:44 +0900

    first

--- src/practice01/prob3/Prob3.java
+++ src/practice01/prob3/Prob3.java
@@ -1,6 +1,24 @@
 package practice01.prob3;
 
+import java.util.Scanner;
+
 public class Prob3 {
 	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력하세요 : ");		
+		int num = sc.nextInt();
+		int result  = 0;
+		if(num%2!=0) {
+			for(int i=1; i<=num; i+=2) {
+				result += i;				
+			}
+		} else {
+			for(int i=0; i<=num; i+=2) {
+				result+= i;
+			}
+		}
+		System.out.println("결과 값 : "+result);
+		sc.close();
 	}
 }
--- src/practice01/prob5/Prob5.java
+++ src/practice01/prob5/Prob5.java
@@ -1,6 +1,17 @@
 package practice01.prob5;
 
 public class Prob5 {
-	public static void main (String args[]) {
+	public static void main(String args[]) {
+		for (int i = 1; i <= 99; i++) {
+			if((i / 10) != 0 && (i / 10) % 3 == 0) {
+				System.out.print(i + " 짝");
+				if((i % 10) % 3 == 0) {
+					System.out.print(" 짝");
+				}
+				System.out.println();
+			} else if((i % 10) != 0 && (i % 10) % 3 == 0) {
+				System.out.println(i + " 짝");
+			}
+		}
 	}
 }
